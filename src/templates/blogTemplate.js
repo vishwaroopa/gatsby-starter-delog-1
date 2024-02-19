1c95aa2a91e9125f9c37ac6db351abb26adb1bf9
vishwaroopa <vishwarupa.vishu@gmail.com>
2024-02-19 16:17:49 +0530

    dfdf

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -11,9 +11,50 @@ export default function Template({
   const { frontmatter, html } = markdownRemark
   return (
     <Layout>
-      <Helmet>
-        <title>{frontmatter.title} | {siteMetadata.title}</title>
+       <Helmet>
+         <title>{frontmatter.title} | {siteMetadata.title}</title>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="author" content="ThemeZaa" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1.0,maximum-scale=1"
+        />
         <meta name="description" content={frontmatter.metaDescription} />
+        <link rel="shortcut icon" href="../images/vider-favicon.png" />
+        <link rel="apple-touch-icon" href="../images/vider-favicon.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="../images/vider-favicon.png"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+          integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+          integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="../images/apple-touch-icon-114x114.png"
+        />
+        <link rel="stylesheet" type="text/css" href="../css/font-icons.min.css" />
+    <link rel="stylesheet" type="text/css" href="../css/theme-vendors.min.css" />
+    <link rel="stylesheet" type="text/css" href="../css/style.css" />
+    <link rel="stylesheet" href="../css/main.css" />
+    <link rel="stylesheet" type="text/css" href="../css/responsive.css" />
+    <script type="text/javascript" src="../js/jquery.min.js"></script>
+    <script type="text/javascript" src="../js/theme-vendors.min.js"></script>
+    <script type="text/javascript" src="../js/main.js"></script>
       </Helmet>
       <div className="blog-post-container">
         <article className="post">
